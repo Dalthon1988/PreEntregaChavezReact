0ebc27d0ccb767d3ab5198e00f1e0684f6353d59
Dalthon1988 <dalthon1988@gmail.com>
2023-06-19 09:47:22 -0300

    PreEntregaChavez

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import './itemlistcontainer.css'
+
+const ItemListContainer = ({saludo}) => { 
+    return (
+        <div className="title">
+            <h1>{saludo}</h1>
+        </div>
+    )
+}
+
+export default ItemListContainer;
